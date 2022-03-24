a6bdc703a47a17f202dbde3ad3e80793cb2751e6
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2022-03-24 17:47:01 +0530

    Update urlreport.sh

--- urlreport.sh
+++ urlreport.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 name=$(echo $1 | awk -F "/" '{print $3}')
-echo -e "URL: $1\n\nResult:" > output/url/$name.url.report
-curl -s --request POST --url https://www.virustotal.com/api/v3/urls --header 'x-apikey: XXXX' --form url=$1 > result1
+echo -e "URL: $1" > output/url/$name.url.report
+function getURLInfo(){
+curl -s --request POST --url https://www.virustotal.com/api/v3/urls --header 'x-apikey: d2d01393e9c34f7d20d08625f1aa6409e8323a9765568dc88a38a2a330213f2f' --form url=$1 > result1
 id=$(cat result1 | jq | grep id | awk -F "\"" '{print $4}')
 rm result1
-curl -s --request GET --url https://www.virustotal.com/api/v3/analyses/$id --header 'x-apikey: XXXX' > result2
+curl -s --request GET --url https://www.virustotal.com/api/v3/analyses/$id --header 'x-apikey: d2d01393e9c34f7d20d08625f1aa6409e8323a9765568dc88a38a2a330213f2f' > result2
+}
+getURLInfo "$1"
+outcome=$(cat result2 | grep -E "harmless|undetected" | grep -v 0 | wc -l)
+if [[ outcome -eq 0 ]] ; then echo -e "[i] API call failed, trying again. Be patient." ; getURLInfo "$1" ; fi
 echo -e "\nAccording to VirusTotal API:"  >> output/url/$name.url.report
 cat result2 | jq | grep -E "harmless|malicious|suspicious|undetected" | grep -iEv "Result|category" | tr -d "\"" | sed 's/\<\([[:lower:]]\)\([[:alnum:]]*\)/\u\1\2/g' >> output/url/$name.url.report
 malicious_sources=$(cat result2 | grep malicious -B 2 | grep -v malicious | grep -E "[[:alpha:]]" | grep -vE "stats|harmless" | tr -d "\":{")
